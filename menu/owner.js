e6f7e7880ed90d8a34fc3273d5d0c4a728af41be
SrAllison52 <78006833+allison5200a@users.noreply.github.com>
2021-03-06 14:50:02 -0300

    Update owner.js

--- menu/owner.js
+++ menu/owner.js
@@ -4,14 +4,14 @@ return `
 
 ╭─────≽「 *REGULATION ${name}* 」
 ┴
-┣⊱  ${a}NAMA USER:${a} *${pushname2}*
-┣⊱  ${a}NAMA GRUP:${a} *${groupName}*
-┣⊱  ${a}VERSION:${a} *0.0.0*
-┣⊱  ${a}USER TERDAFTAR:${a} *${user.length} User*
+┣⊱  ${a}NOME DE USUÁRIO:${a} *${pushname2}*
+┣⊱  ${a}NOME DO GRUPO:${a} *${groupName}*
+┣⊱  ${a}VERSÃO:${a} *0.0.0*
+┣⊱  ${a}USUÁRIO REGISTRADO:${a} *${user.length} User*
 ┬
 ╰────────────────────────
 
-╭────────≽「 *OWNER ONLY* 」
+╭────────≽「 *SÓ PROPRIETÁRIO* 」
 ┴
 ┠≽ *${prefix}addstatus*
 ┠≽ *${prefix}addbadword*
